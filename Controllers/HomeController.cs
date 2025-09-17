abfe40b7f9b1c37c628e9beb73a9c2ece4ad35a2
Arash <arash8877@gmail.com>
2025-09-17 14:40:26 +0200

    display products in the home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,30 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CodeWithArash.Models;
+using CodeWithArash.Data;
 
 namespace CodeWithArash.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private CodeWithArashContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, CodeWithArashContext context) // constructor with dependency injection
     {
         _logger = logger;
+        _context = context; // dependency injection of the database context
     }
 
     public IActionResult Index()
     {
-        return View();
+        var products = _context.Products.ToList(); // fetch all products from the database
+        return View(products); // pass the list of products to the view
+    }
+
+    public IActionResult ProductDetails(int id)
+    {
+        return null;
     }
 
     public IActionResult ContactUs()

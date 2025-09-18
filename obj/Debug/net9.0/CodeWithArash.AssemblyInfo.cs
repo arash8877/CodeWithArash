f0a9f95d7f4b86c457caa2e6b49b100ac0c63d5d
Arash <arash8877@gmail.com>
2025-09-18 13:07:50 +0200

    improve the ui for basket page

--- obj/Debug/net9.0/CodeWithArash.AssemblyInfo.cs
+++ obj/Debug/net9.0/CodeWithArash.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeWithArash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0402c6ed09e24a3f8ebe377af2aabe1d00b61032")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd65d0262d9a81dcee5b6fb0963751890e0e1aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeWithArash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeWithArash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
